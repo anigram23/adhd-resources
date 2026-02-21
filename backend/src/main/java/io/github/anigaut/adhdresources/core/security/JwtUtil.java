370778407fa6d8c997c2be5f762e5cf215ea5491
anirudh <anirudhgr23@gmail.com>
2026-02-21 14:22:41 +0530

    Generate JWT

---
+++ backend/src/main/java/io/github/anigaut/adhdresources/core/security/JwtUtil.java
@@ -0,0 +1,34 @@
+package io.github.anigaut.adhdresources.core.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    private SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expirationTime))
+                .signWith(getSecretKey())
+                .compact();
+    }
+}
