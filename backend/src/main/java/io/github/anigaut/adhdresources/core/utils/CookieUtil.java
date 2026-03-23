b3dc7b5a3198aefc9886757d3dacdb9e8d65947d
anirudh <anirudhgr23@gmail.com>
2026-03-23 14:58:23 +0530

    Cookie utility to attach JWT to HTTP cookie in response header

---
+++ backend/src/main/java/io/github/anigaut/adhdresources/core/utils/CookieUtil.java
@@ -0,0 +1,23 @@
+package io.github.anigaut.adhdresources.core.utils;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    public void attachJwtCookie(HttpServletResponse response, String token) {
+        ResponseCookie cookie = ResponseCookie.from("jwt", token)
+                .httpOnly(true)
+                .secure(true)
+                .path("/")
+                .maxAge(expiration / 1000) // Convert milliseconds to seconds
+                .sameSite("Strict")
+                .build();
+        response.addHeader("Set-Cookie", cookie.toString());
+    }
+}
