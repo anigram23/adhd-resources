664d563477d0efbdae04ad0e485eab9be9bebd75
anirudh <anirudhgr23@gmail.com>
2026-03-28 23:27:27 +0530

    Configured Chakra UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import router from './routing/Router'
+import { Provider } from './components/ui/provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 )
