cca662be52ff59b35fa39662d37edd726f2213d4
anirudh <anirudhgr23@gmail.com>
2026-04-01 11:38:54 +0530

    Modified layout to include navbar on all pages

---
+++ frontend/src/routing/Layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar/Navbar";
+import { Outlet } from "react-router";
+
+export default function Layout() {
+    return (
+        <>
+            <Navbar />
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+}
--- frontend/src/routing/Router.tsx
+++ frontend/src/routing/Router.tsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter } from "react-router"
 import Home from "../pages/Home"
+import Layout from "./Layout"
 
 const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
+    { 
+        path: "/", 
+        element: <Layout /> ,
+        children: [
+            { index: true, element: <Home /> }
+        ]
+    },
 ])
 
 export default router
