87937d258ae58eb8169fc80825c86442235a8475
anirudh <anirudhgr23@gmail.com>
2026-04-07 15:25:32 +0530

    Added Tanstack query to project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import router from './routing/Router'
 import { Provider } from './components/ui/provider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 )
