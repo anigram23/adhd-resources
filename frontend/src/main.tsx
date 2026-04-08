9f8782e54a5723757fadd07affd640183323f12a
anirudh <anirudhgr23@gmail.com>
2026-04-08 21:56:10 +0530

    Added auth context to entire app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router'
 import router from './routing/Router'
 import { Provider } from './components/ui/provider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {AuthProvider} from "@/AuthContext.tsx";
 
 const queryClient = new QueryClient();
 
@@ -11,7 +12,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+          <AuthProvider>
+              <RouterProvider router={router} />
+          </AuthProvider>
       </QueryClientProvider>
     </Provider>
   </StrictMode>
