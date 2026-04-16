c1c236730064fdf8b1b0e1ad07fe9142b1b17251
anirudh <anirudhgr23@gmail.com>
2026-04-16 15:49:15 +0530

    Logout (admin and reviewer) completed

--- frontend/src/components/navbar/Links.tsx
+++ frontend/src/components/navbar/Links.tsx
@@ -1,5 +1,9 @@
 import { Button, HStack, VStack } from "@chakra-ui/react";
 import {useAuth} from "@/auth/AuthContext.tsx";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {adminLogout} from "@/api_service/admin.ts";
+import {useNavigate} from "react-router";
+import {reviewerLogout} from "@/api_service/reviewer.ts";
 
 const links = [
     { name: "Home", path: "/" },
@@ -12,8 +16,40 @@ type LinksProps = {
 };
 
 export default function Links({ isMobile = true }: LinksProps) {
-    const { isAuthenticated } = useAuth();
+    const { isAuthenticated, user } = useAuth();
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
     const StackType = isMobile ? VStack : HStack;
+
+
+    const handleLogout = () => {
+        // @ts-ignore
+        const role = user?.role;
+
+        if (role === "ADMIN") {
+            adminLogoutMutation.mutate();
+        } else if (role === "REVIEWER") {
+            reviewerLogoutMutation.mutate();
+        }
+    }
+
+    const adminLogoutMutation = useMutation({
+        mutationFn: adminLogout,
+        onSuccess: () => {
+            queryClient.setQueryData(["currentUser"], null);
+            // queryClient.removeQueries({ queryKey: ["currentUser"] });
+            navigate("/admin/login");
+        }
+    });
+
+    const reviewerLogoutMutation = useMutation({
+        mutationFn: reviewerLogout,
+        onSuccess: () => {
+            queryClient.setQueryData(["currentUser"], null);
+            navigate("/");
+        }
+    })
+
     return (
         <StackType gap={isMobile ? 4 : 8} align={isMobile ? "start" : "center"}>
             {links.map(link => (
@@ -21,7 +57,7 @@ export default function Links({ isMobile = true }: LinksProps) {
             ))}
 
             {isAuthenticated ? (
-                <Button variant="outline">Logout</Button>
+                <Button variant="outline" onClick={handleLogout}>Logout</Button>
             ): (
                 <>
                     <Button variant="outline"><a href="/login">Login</a></Button>
