ee7693d97c23d394008c96a6afacf91c8f04db8b
anirudh <anirudhgr23@gmail.com>
2026-05-18 19:52:30 +0530

    Professional mapper and service created to add new professional from a review

--- backend/src/main/java/io/github/anigaut/adhdresources/professional/ProfessionalMapper.java
+++ backend/src/main/java/io/github/anigaut/adhdresources/professional/ProfessionalMapper.java
@@ -1,17 +1,24 @@
 package io.github.anigaut.adhdresources.professional;
 
+import io.github.anigaut.adhdresources.city.City;
 import io.github.anigaut.adhdresources.professional.dto.ProfessionalRequestDTO;
 import io.github.anigaut.adhdresources.professional.dto.ProfessionalResponseDTO;
 import io.github.anigaut.adhdresources.professional.dto.ProfessionalUpdateDTO;
+import io.github.anigaut.adhdresources.professionalType.ProfessionalType;
 import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface ProfessionalMapper {
 
-    @Mapping(target = "averageRating", ignore = true)
-    Professional toEntity(ProfessionalRequestDTO dto);
+    @Mapping(target = "city", source = "city")
+    @Mapping(target = "professionalType", source = "professionalType")
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "name", source = "dto.name")
+    Professional toEntity(ProfessionalRequestDTO dto, City city, ProfessionalType professionalType);
 
-    @Mapping(target = "averageRating", ignore = true)
     ProfessionalResponseDTO toResponseDTO(Professional entity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
--- backend/src/main/java/io/github/anigaut/adhdresources/professional/ProfessionalService.java
+++ backend/src/main/java/io/github/anigaut/adhdresources/professional/ProfessionalService.java
@@ -1,10 +1,10 @@
 package io.github.anigaut.adhdresources.professional;
 
+import io.github.anigaut.adhdresources.city.City;
 import io.github.anigaut.adhdresources.city.CityRepository;
 import io.github.anigaut.adhdresources.core.exception.HttpException;
 import io.github.anigaut.adhdresources.professional.dto.ProfessionalRequestDTO;
-import io.github.anigaut.adhdresources.professional.dto.ProfessionalResponseDTO;
-import io.github.anigaut.adhdresources.professional.dto.ProfessionalUpdateDTO;
+import io.github.anigaut.adhdresources.professionalType.ProfessionalType;
 import io.github.anigaut.adhdresources.professionalType.ProfessionalTypeRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -20,41 +20,14 @@ public class ProfessionalService {
     private final ProfessionalTypeRepository professionalTypeRepository;
 
     @Transactional
-    public ProfessionalResponseDTO createProfessional(ProfessionalRequestDTO professionalRequestDTO) {
+    public Professional createProfessionalFromReview(ProfessionalRequestDTO professionalRequestDTO) {
 
-        if (!cityRepository.existsById(professionalRequestDTO.getCityId())) {
-            throw new HttpException(HttpStatus.NOT_FOUND, "City not found");
-        }
+        City city = cityRepository.findById(professionalRequestDTO.getCityId())
+                .orElseThrow(() -> new HttpException(HttpStatus.BAD_REQUEST, "City not found"));
 
-        if (!professionalTypeRepository.existsById(professionalRequestDTO.getProfessionalTypeId())) {
-            throw new HttpException(HttpStatus.NOT_FOUND, "Professional type not found");
-        }
+        ProfessionalType professionalType = professionalTypeRepository.findById(professionalRequestDTO.getProfessionalTypeId())
+                .orElseThrow(() -> new HttpException(HttpStatus.BAD_REQUEST, "Professional type not found"));
 
-        Professional newProfessional = professionalRepository.save(professionalMapper.toEntity(professionalRequestDTO));
-        return professionalMapper.toResponseDTO(newProfessional);
-    }
-
-    @Transactional
-    public ProfessionalResponseDTO updateProfessional(int id, ProfessionalUpdateDTO  professionalUpdateDTO) {
-        Professional professional = professionalRepository.findById(id).orElseThrow(
-                () -> new HttpException(HttpStatus.NOT_FOUND, "Professional not found"));
-
-        if (professionalUpdateDTO.getCityId() != null &&  !cityRepository.existsById(professionalUpdateDTO.getCityId())) {
-            throw new HttpException(HttpStatus.NOT_FOUND, "City not found");
-        }
-
-        professionalMapper.updateEntity(professionalUpdateDTO, professional);
-        professionalRepository.save(professional);
-        return professionalMapper.toResponseDTO(professional);
-    }
-
-    @Transactional
-    public String deleteProfessional(int id) {
-        Professional professional = professionalRepository.findById(id).orElseThrow(
-                () -> new HttpException(HttpStatus.NOT_FOUND, "Professional not found")
-        );
-
-        professionalRepository.delete(professional);
-        return "Deleted Professional Successfully";
+        return professionalRepository.save(professionalMapper.toEntity(professionalRequestDTO, city, professionalType));
     }
 }
