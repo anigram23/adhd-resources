89f19c45dbb860ca70831bda0576eaabbe233ee5
anirudh <anirudhgr23@gmail.com>
2026-05-27 14:35:31 +0530

    Added reviewer details, and creation and updation times to reviewer DTO

--- backend/src/main/java/io/github/anigaut/adhdresources/review/dto/ReviewResponseDTO.java
+++ backend/src/main/java/io/github/anigaut/adhdresources/review/dto/ReviewResponseDTO.java
@@ -1,10 +1,13 @@
 package io.github.anigaut.adhdresources.review.dto;
 
+import io.github.anigaut.adhdresources.core.security.auth.UserDetailsDTO;
 import io.github.anigaut.adhdresources.professional.dto.ProfessionalResponseDTO;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.Instant;
 
 @Getter
 @Setter
@@ -13,6 +16,7 @@ import lombok.Setter;
 public class ReviewResponseDTO {
     private int id;
     private ProfessionalResponseDTO professional;
+    private UserDetailsDTO reviewer;
     private String content;
     private float rating;
     private String contactNumber;
@@ -20,4 +24,6 @@ public class ReviewResponseDTO {
     private int consultationFee;
     private Integer diagnosisFee;
     private boolean doesOnlineConsultations;
+    private Instant createdAt;
+    private Instant updatedAt;
 }
