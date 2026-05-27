ba262657e40433328d4580810838d8c01ae702da
anirudh <anirudhgr23@gmail.com>
2026-05-27 11:02:38 +0530

    Modified FullPageLoader styling.

--- frontend/src/components/utils/FullPageLoader.tsx
+++ frontend/src/components/utils/FullPageLoader.tsx
@@ -1,20 +1,16 @@
-import {Flex, Heading, Spinner, VStack, Text } from "@chakra-ui/react";
+import {Flex, Spinner, Text, VStack} from "@chakra-ui/react";
 
 export default function FullPageLoader() {
     return (
-        <Flex
-            minH="100vh"
-            align="center"
-            justify="center"
-        >
+        <Flex minH="100vh" align="center" justify="center" bg="blue.50">
             <VStack gap={4}>
-                <Heading size="md">ADHD Resources</Heading>
-
-                <Spinner size="lg" borderWidth="4px" animationDuration="0.65s" />
-
-                <Text fontSize="lg">
-                    Loading...
-                </Text>
+                <Spinner
+                    size="lg"
+                    borderWidth="4px"
+                    animationDuration="0.65s"
+                    colorPalette="blue"
+                />
+                <Text fontSize="lg" color="gray.700">Loading...</Text>
             </VStack>
         </Flex>
     )
